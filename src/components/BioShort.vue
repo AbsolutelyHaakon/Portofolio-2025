ddbf37999249e07e83241abb8b34e89de6956c18
HSK <haakonsk@live.no>
2025-03-23 17:56:55 +0100

    Initial Commit

---
+++ src/components/BioShort.vue
@@ -0,0 +1,121 @@
+<script setup>
+const profileImage = 'src/assets/images/qualifications/profilePic.jpg';
+const name = 'Håkon Svensen Karlsen';
+const title = 'Bachelor i ingeniørfag, data';
+const city = 'NTNU Ålesund';
+const email = "haakonsk@live.no";
+const qualifications = [
+  {name: 'Flutter' ,image: 'src/assets/images/qualifications/flutter.png'},
+  { name: 'Vue.js', image: 'src/assets/images/qualifications/vue.png' },
+  { name: 'JavaScript', image: 'src/assets/images/qualifications/js.png' },
+  { name: 'HTML', image: 'src/assets/images/qualifications/html.png' },
+  { name: 'CSS', image: 'src/assets/images/qualifications/css.png' },
+  {name: 'TS', image: 'src/assets/images/qualifications/ts.png'},
+  {name: 'SQL', image: 'src/assets/images/qualifications/sql.png'},
+];const biography = 'Student på 24 år som fullfører bachelor ved NTNU Ålesund. God erfaring fra en rekke prosjekter og praksis i IT-Firma. Denne siden detaljerer prosjektene og arbeidserfaringen jeg mener er relevant for fremtidig arbeid. Oppdateringer på ulike prosjekter finnes også her.';
+</script>
+
+<template>
+  <div class="bio-box">
+    <div class="left">
+      <img :src="profileImage" alt="Profile Image" class="profile-image" />
+      <h2>{{ name }}</h2>
+      <div style="gap:1px ">
+        <h3 class="UT">{{ title }}</h3>
+        <p class="UT">{{ city }}</p>
+        <p class="UT">{{email}}</p>
+      </div>
+    </div>
+    <div class="right">
+    <p class="bio">{{ biography }}</p>
+    </div>
+  </div>
+  <div class="qualification-box">
+    <h4 style="margin-left: 15px">Hovedkvalifikasjoner</h4>
+    <ul class="strengths">
+      <li v-for="qualification in qualifications" :key="qualification.name">
+        <img :src="qualification.image" :alt="qualification.name" class="strengths-image" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.bio-box {
+  display: flex;
+  border-radius: 10px;
+  padding: 20px;
+  max-width: 900px;
+  margin: 0 auto;
+  height: 400px;
+}
+
+.left {
+  flex: 1;
+  text-align: center;
+}
+
+.profile-image {
+  width: 180px;
+  height: 180px;
+  border-radius: 50%;
+  object-fit: cover;
+  margin-bottom: 10px;
+}
+
+.right {
+  flex: 2;
+  padding-left: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+h2, h3, h4 {
+  margin: 5px 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+ul li {
+  margin-bottom: 5px;
+}
+
+.strengths-image {
+  width: 45px;
+  height: 45px;
+}
+
+.strengths-image:hover {
+  transform: scale(1.4);
+}
+
+.strengths {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  min-width: 100%;
+  padding: 0 20px;
+}
+
+.qualification-box {
+  display: flex;
+  justify-items: center;
+  margin: auto;
+  gap: 10px;
+  flex-direction: column;
+  max-width: 1000px;
+}
+
+.bio {
+  margin: 100px 15px 0 15px;
+}
+
+.UT {
+  text-align: left;
+  margin: 0 0 0 25px;
+}
+</style>
---
+++ src/components/Timeline.vue
@@ -0,0 +1,167 @@
+<script setup>
+const timelineItems = [
+  {
+    date: 'Vår 2025',
+    title: 'Bacheloroppgaven - Warehouse Workflow Simulator',
+    image: 'src/assets/images/projects/wws.png',
+    description: 'Bachelorprosjekt i samarbeid med Solwr Solutions AS. Anvender Monte Carlo simulering og maskinlæring for å optimalisere lagerflyt.',
+  },
+  {
+    date: '2025 - Nå',
+    title: 'Afasia - Spesialtilpasset treningsapplikasjon',
+    image: 'src/assets/images/projects/afasia.png',
+    description: 'Prosjekt i samarbeid med bachelorgruppen. Egenutviklet applikasjon for trening spesiallaget for afasipasienter eller andre med lignende treningsbehov.',
+  },
+  {
+    date: '2019-2020',
+    title: 'Project C',
+    image: 'src/assets/images/projectC.jpg',
+    description: 'Description for Project C.',
+  },
+  {
+    date: '2018-2019',
+    title: 'Project D',
+    image: 'src/assets/images/projectD.jpg',
+    description: 'Description for Project D.',
+  },
+  {
+    date: '2017-2018',
+    title: 'Project E',
+    image: 'src/assets/images/projectE.jpg',
+    description: 'Description for Project E.',
+  },
+  // Add more items as needed
+];
+</script>
+
+<template>
+  <div class="timeline">
+    <div v-for="(item, index) in timelineItems" :key="index"
+         :class="['timeline-item', { 'timeline-item-right': index % 2 === 0 }]">
+      <div class="timeline-content">
+        <div class="left">
+          <img :src="item.image" alt="Timeline Image" class="timeline-image"/>
+        </div>
+        <div class="right">
+          <h3>{{ item.title }}</h3>
+          <p class="timeline-date">{{ item.date }}</p>
+          <p>{{ item.description }}</p>
+          <a href="#" class="view-more">Lær Mer</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.timeline {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  max-width: 1300px;
+  margin: 0 auto;
+  padding: 20px 0;
+}
+
+.timeline-item {
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+  padding: 20px;
+  position: relative;
+}
+
+.timeline-item:nth-child(odd) {
+  justify-content: flex-start;
+}
+
+.timeline-item::before {
+  content: '';
+  position: absolute;
+  width: 30%;
+  height: 3px;
+  background: repeating-linear-gradient(
+    90deg,
+    #A24936,
+    #A24936 10px,
+    #ffffff 10px,
+    #ffffff 20px
+  );
+  top: 50%;
+  transform: translateY(-50%);
+  z-index: -1;
+}
+
+.timeline-item:nth-child(even)::before {
+  left: 50%;
+}
+
+.timeline-item:nth-child(odd)::before {
+  right: 50%;
+}
+
+.timeline::after {
+  content: '';
+  position: absolute;
+  width: 3px;
+  background-color: #2C363F;
+  top: 0;
+  bottom: 0;
+  left: 50%;
+  margin-left: -3px;
+}
+
+.timeline-content {
+  width: 45%;
+  padding: 20px;
+  background-color: #D6DBD2;
+  position: relative;
+  border-radius: 15px;
+  display: flex;
+  gap: 30px;
+}
+
+.timeline-item:nth-child(even) .timeline-content {
+  flex-direction: row-reverse;
+  text-align: right;
+  padding-bottom: 40px;
+}
+
+.timeline-item:nth-child(even) .view-more {
+  margin-top:10px;
+  text-align: right;
+  position: absolute;
+  bottom: 15px;
+  left: 20px;
+}
+
+.left {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #2C363F;
+  border-radius: 12px;
+  margin: auto;
+}
+
+.timeline-image {
+  width: 100px;
+  height: auto;
+  border-radius: 6px;
+  object-fit: cover;
+}
+
+.timeline-date {
+  font-size: 14px;
+  color: #888;
+}
+
+.view-more {
+  display: block;
+  text-align: right;
+  color: #A24936;
+  text-decoration: none;
+  margin-top: 10px;
+}
+</style>
